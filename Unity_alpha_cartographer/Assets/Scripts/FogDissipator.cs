407383c6d1d13f6b974eb3c585aa8a3bb26d7c50
Alvin Herawan <alvin.herawan@idmaster.eu>
2016-07-09 22:09:09 +0800

    Extracting Alpha Version
    From unitypackage, testing the gitignore file to determine which file is
    needed for Unity to work well accross different computers.

---
+++ Unity_alpha_cartographer/Assets/Scripts/FogDissipator.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FogDissipator: MonoBehaviour {
+
+	public float dissipateTime = 0.5f;
+
+	private bool startDissipate = false;
+	private float dissipateTimeTemp;
+
+	void Start () 
+	{
+		dissipateTimeTemp = dissipateTime;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(startDissipate)
+		{
+			dissipateTimeTemp = dissipateTimeTemp - Time.deltaTime;
+			if(dissipateTimeTemp <= 0)
+			{
+				this.GetComponent<ParticleSystem>().emissionRate = 0;
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject.tag == "Projectile")
+		{
+			this.GetComponent<ParticleSystem>().loop = false;
+			this.GetComponent<ParticleSystem>().emissionRate = 0.5f;
+			startDissipate = true;
+		}
+	}
+}
